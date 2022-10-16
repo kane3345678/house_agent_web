eb7963ac75d76cf091f906db7242cd0eadb88dfb
Kane <kane3345678@gmail.com>
2022-10-16 11:39:49 +0800

    Remove unused code

--- mongo.py
+++ mongo.py
@@ -30,29 +30,3 @@ class MongoDB():
 
     def delete_one(self, mongo_scipt):
         return self.collection.delete_one(mongo_scipt)
-'''
-
-
-import datetime
-db = MongoDB("mongodb://localhost:27017/", "house", "house_hist")
-#db.insert_data({"house_name": "", "price": 50, "address": "新北市中和區","date":datetime.datetime(2022, 10, 8, 11)})
-#db.insert_data({"house_name": "", "price": 50, "address": "新北市中和區","date":datetime.datetime(2022, 10, 9, 11)})
-d = datetime.datetime(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day, datetime.datetime.now().hour)
-print(d)
-data = db.find_data({"date": {"$eq": d}})
-for i in data:
-    print(i)
-exit(0)
-
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["house"]
-dblst = myclient.list_database_names()
-mycol = mydb["house"]
-collst = mydb.list_collection_names()
-if "house" in collst:
-  print("testMongoCol集合已存在！")
-  mytestData = { "name": "Allen", "gender": "male", "address": "苗栗縣苑裡鎮" }
-  x = mycol.insert_one(mytestData)
-  x = mycol.find_one()
-print(x)
-'''
