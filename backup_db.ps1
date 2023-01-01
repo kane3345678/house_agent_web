2afaa9abbc49898126285eae4f3d15b9155ccda5
Kane <kane3345678@gmail.com>
2023-01-01 19:33:13 +0800

    add more collection backup

--- backup_db.ps1
+++ backup_db.ps1
@@ -2,3 +2,5 @@ $timestamp = Get-Date -Format "MM-dd-yyyy-HH-mm"
 mongoexport --uri="mongodb://localhost:27017" /d house /c house_hist --out=db_backup/house_db_house_hist_$timestamp.json
 mongoexport --uri="mongodb://localhost:27017" /d house /c new_house --out=db_backup/house_db_new_house_$timestamp.json
 mongoexport --uri="mongodb://localhost:27017" /d house /c price_drop --out=db_backup/house_db_price_drop_$timestamp.json
+mongoexport --uri="mongodb://localhost:27017" /d house /c deal --out=db_backup/house_db_deal_$timestamp.json
+mongoexport --uri="mongodb://localhost:27017" /d house /c deal_cpking --out=db_backup/house_db_deal_$timestamp.json
