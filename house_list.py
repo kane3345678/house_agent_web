b0035b68f864c9a5d2d950f47435e63144fdf642
kane.chen <kane3345678@gmail.com>
2023-04-08 13:46:11 +0800

    add more district

--- house_list.py
+++ house_list.py
@@ -12,7 +12,7 @@ all_agents = [
                 {"url":"https://buy.yungching.com.tw/region/%E6%96%B0%E5%8C%97%E5%B8%82-%E6%9D%BF%E6%A9%8B%E5%8D%80_c/2000-6000_price/", "region":"板橋"},
                 {"url":"https://buy.yungching.com.tw/region/%E5%8F%B0%E5%8C%97%E5%B8%82-%E5%8D%97%E6%B8%AF%E5%8D%80_c/2000-6000_price/", "region":"南港"},
                 {"url":"https://buy.yungching.com.tw/region/%E5%8F%B0%E5%8C%97%E5%B8%82-%E5%A3%AB%E6%9E%97%E5%8D%80_c/2000-6000_price/", "region":"士林"},
-                {"url":"https://buy.yungching.com.tw/region/%E5%8F%B0%E5%8C%97%E5%B8%82-%E5%A3%AB%E6%9E%97%E5%8D%80_c/2000-6000_price/", "region":"中正"}
+                {"url":"https://buy.yungching.com.tw/region/新竹縣-竹北市_c/1500-6000_price/", "region":"竹北"}
 
             ]
         },
@@ -25,7 +25,8 @@ all_agents = [
                 {"url":"https://www.sinyi.com.tw/buy/list/2000-6000-price/NewTaipei-city/220-zip/Taipei-R-mrtline/03-mrt/default-desc/", "region":"板橋"},
                 {"url":"https://www.sinyi.com.tw/buy/list/2000-6000-price/Taipei-city/115-zip/Taipei-R-mrtline/03-mrt/default-desc/","region":"南港"},
                 {"url":"https://www.sinyi.com.tw/buy/list/2000-6000-price/Taipei-city/111-zip/Taipei-R-mrtline/03-mrt/default-desc/","region":"士林"},
-                {"url":"https://www.sinyi.com.tw/buy/list/2000-6000-price/Taipei-city/100-zip/Taipei-R-mrtline/03-mrt/default-desc/","region":"中正"}
+                {"url":"https://www.sinyi.com.tw/buy/list/2000-6000-price/Taipei-city/100-zip/Taipei-R-mrtline/03-mrt/default-desc/","region":"中正"},
+                {"url":"https://www.sinyi.com.tw/buy/list/2000-6000-price/Hsinchu-county/302-zip/Taipei-R-mrtline/03-mrt/default-desc/","region":"竹北"},
             ]
         }
 ]
@@ -38,11 +39,12 @@ gov_deal_591 = [
 ]
 
 gov_deal_cpking = [
+    {"url":"https://community.houseprice.tw/ws/list/新竹縣_city/竹北市_zip", "district":"新竹縣竹北市"},
+    {"url":"https://community.houseprice.tw/ws/list/台北市_city/北投區_zip", "district":"台北市北投區"},
     {"url":"https://community.houseprice.tw/ws/list/%E6%96%B0%E5%8C%97%E5%B8%82_city/%E6%B0%B8%E5%92%8C%E5%8D%80_zip", "district":"新北市永和區"},
     {"url":"https://community.houseprice.tw/ws/list/%E6%96%B0%E5%8C%97%E5%B8%82_city/%E4%B8%AD%E5%92%8C%E5%8D%80_zip", "district":"新北市中和區"},
     {"url":"https://community.houseprice.tw/ws/list/%E6%96%B0%E5%8C%97%E5%B8%82_city/%E6%9D%BF%E6%A9%8B%E5%8D%80_zip", "district":"新北市板橋區"},
     {"url":"https://community.houseprice.tw/ws/list/%E6%96%B0%E5%8C%97%E5%B8%82_city/%E6%96%B0%E8%8E%8A%E5%8D%80_zip", "district":"新北市新莊區"},
     {"url":"https://community.houseprice.tw/ws/list/%E6%96%B0%E5%8C%97%E5%B8%82_city/%E6%96%B0%E5%BA%97%E5%8D%80_zip", "district":"新北市新店區"},
     {"url":"https://community.houseprice.tw/ws/list/%E6%96%B0%E5%8C%97%E5%B8%82_city/%E6%9E%97%E5%8F%A3%E5%8D%80_zip", "district":"新北市林口區"},
-
 ]
